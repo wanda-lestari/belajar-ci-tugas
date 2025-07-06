666fe696f8a0faf97fb574f9528c31a31cdaa55d
wanda-lestari <wandalestari910@gmail.com>
2025-07-06 19:34:32 +0700

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,30 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login', ['filter' => 'redirect']);
+$routes->get('logout', 'AuthController::logout');
+
+$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->post('produk', 'ProdukController::create', ['filter' => 'auth']);
+$routes->post('produk/edit/(:any)', 'ProdukController::edit/$1', ['filter' => 'auth']);
+$routes->get('produk/delete/(:any)', 'ProdukController::delete/$1', ['filter' => 'auth']);
+$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
+
+$routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'TransaksiController::index');
+    $routes->post('', 'TransaksiController::cart_add');
+    $routes->post('edit', 'TransaksiController::cart_edit');
+    $routes->get('delete/(:any)', 'TransaksiController::cart_delete/$1');
+    $routes->get('clear', 'TransaksiController::cart_clear');
+});
+$routes->get('faq', 'Home::faq', ['filter' => 'auth']);
+$routes->get('profile', 'Home::profile', ['filter' => 'auth']);
+$routes->get('contact', 'Home::contact', ['filter' => 'auth']);
---
+++ app/Controllers/Home.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ProductModel; 
+
+class Home extends BaseController
+{
+    protected $product;
+
+    function _construct(){
+        $this->product = new ProductModel();
+
+    }
+    public function index(): string
+    {
+        $product = $this->product->findAll();
+        $data['product'] = $product;
+        return view('v_home', $data);
+    }
+
+    public function faq()
+    {
+        return view('v_faq');
+    }
+
+    public function profile()
+    {
+        return view('v_profile');
+    }
+
+    public function contact()
+    {
+        return view('v_contact');
+    }
+}
